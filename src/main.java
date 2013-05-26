0cf98467bf1a28972f61503d640afc684ac112ff
Serena Ziviani <senseriumi@gmail.com>
2013-05-26 12:35:18 +0200

    Fixed merge conflict in src/main.java

--- src/main.java
+++ src/main.java
@@ -6,23 +6,12 @@ import javax.swing.JFrame;
 
 public class main {
 
-        /**
-         * @param args
-         */
-        public static void main(String[] args) {
-                // TODO Auto-generated method stub
-                JFrame board = new JFrame();
-                Container c = new Container();
-                c = board.getContentPane();
-                ChessBoard cboard = new ChessBoard();
-                //FIXME: i want to move this MinimumSize to the ChessBoard object
-                board.setMinimumSize(new Dimension(20,20));
-                c.add(cboard);
-
-                board.pack();
-                board.setVisible(true);
-
-                board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Window wind = new Window();
+	}
 
 }

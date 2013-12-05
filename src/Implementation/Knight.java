b52d7605d972d75efbac938a104644ca33db978a
Serena Ziviani <senseriumi@gmail.com>
2013-12-05 12:39:29 +0100

    [src/Implementation/Knight.java] Correct the isValid condition

--- src/Implementation/Knight.java
+++ src/Implementation/Knight.java
@@ -13,7 +13,7 @@ public class Knight extends Piece {
 	public boolean isValid(int nX, int nY) {
 		int distX = Math.abs(x - nX);
 		int distY = Math.abs(y - nY);
-		if ((distY + distX) == 3)
+		if ((distX == 1 && distY == 2) || (distX == 2 && distY == 1))
 			return true;
 		return false;
 	}
